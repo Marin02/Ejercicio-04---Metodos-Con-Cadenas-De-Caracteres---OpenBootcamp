5edc35936ebb8598a273c5ee071abe677433b1b3
Juan Felipe Marin <102315395+Marin02@users.noreply.github.com>
2023-04-04 23:13:15 -0500

    Add files via upload

---
+++ Ejercicio 04.js
@@ -0,0 +1,23 @@
+// Ejercicio 04 - Metodos con cadenas de caracteres
+
+let nombre = "Juan Felipe";
+let apellido = "Marin Molina";
+
+let estudiante = `${nombre} ${apellido}`;
+
+let estudianteMayus = estudiante.toUpperCase();
+
+let estudianteMinus = estudiante.toLowerCase();
+
+let num_letras = estudiante.length;
+
+let primera_letra_nombre = nombre.charAt(0);
+
+let ultima_letra_apellido = apellido.charAt(11);
+
+let eliminar_espacios = estudiante.trim();
+
+let boolean = estudiante.includes(nombre);
+
+
+
